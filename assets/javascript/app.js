5ae951ebc339d22886b017f2a0a8f3147aa5f663
ann-leonard <aleo0779@gmail.com>
2019-03-10 22:17:56 -0400

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,43 @@
 var questionList = [
     {
-        question: 'test',
-        options: ['a', 'b', 'c', 'd'],
-        answer: 'd'
+        question: 'What is the first line said in "The Office"?',
+        options: ['"All right Jim, your quarterlies look very good."', '"People say I am the best boss."', '"Dunder Mifflin, this is Pam"', '"Any messages?"'],
     },
     {
-        question: 'test2',
-        options: ['a', 'b', 'c', 'd'],
-        answer: 'd'
+        question: 'According to Dwight, nostalgia is one of the greatest human weaknesses, second only to what?',
+        options: ['the neck', 'soft bellies', 'emotion', 'weak eyesight'],
     },
     {
-        question: 'test3',
-        options: ['a', 'b', 'c', 'd'],
-        answer: 'd'
+        question: "Which of these is one of Ryan's companies?",
+        options: ['Wupf', 'Twitcog', 'Silicorp', 'Blogy'],
+    },
+    {
+        question: "Why does Michael grill his foot?",
+        options: ['He likes the smell of bacon in the morning', 'He was trying to show off', 'He lost a bet with Packer', 'To show the grill was not hot']
+    },
+    {
+        question: "What is Erin's real name?",
+        options: ['Kelly', 'Angela', 'Rebecca', 'Monica']
+    },
+    {
+        question: "Who is Plop?",
+        options: ["Pete", "Andy", "Clark", "Toby"]
+    },
+    {
+        question: "Where was Erin's room when she was growing up?",
+        options: ["Her hair", "A broom closet", "The inside of her sweatshirt", "At the top of a stone tower"]
+    },
+    {
+        question: "Which is NOT one of Jim's pranks?",
+        options: ["He programmed the 9 on Dwight's phone to speed-dial Hong Kong", "Convinces Dwight to buy magic beans", "Linked his headset to Dwight's phone", "Lego Desk"]
+    },
+    {
+        question:"What does Todd Packer's license plate say?",
+        options:["WLHUNG","BGWILLY","BLZDEEP","L8RLSR"]
+    },
+    {
+        question: "Which of Michael's comedy performances resulted in 'Diversity Day'?",
+        options: ["the Chris Rock routine","Ping","Bill Cosby impression","Using Darryl's made up lingo"]
     }
 ]
 var score = 0;
@@ -20,11 +45,15 @@ var quesInc = -1;
 var answerBtns = $(".answerBtn");
 answerBtns.hide();
 $("#timer").hide();
-var number = 16;
+var number = 31;
 var intervalId;
+function setup() {
+    for (i = 0; i < questionList.length; i++) {
+        questionList[i].answer = questionList[i].options[0]
+    }
+}
+setup()
 
-
-//shuffles the items in an array
 function randomShuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -67,7 +96,7 @@ function gradeInput() {
 
     setTimeout(render, 1500);
     clearInterval(intervalId);
- 
+
 }
 
 
@@ -102,16 +131,17 @@ function endGame() {
     answerBtns.hide()
     $("h1").text("Quiz over!");
     $("h3").text(`Your score is ${score}/${questionList.length}`);
-    $("h4").text(`Percentage correct: ${score / questionList.length}`);
+    $("h4").text(`Percentage correct: ${(score / questionList.length).toFixed(2)}`);
     clearInterval(intervalId);
 }
 
 
 
 function render() {
-   
     $("#welcome").empty()
-    answerBtns.show()
+
+    answerBtns.show();
+
     for (i = 0; i < answerBtns.length; i++) {
         document.getElementsByClassName('answerBtn')[i].style.backgroundColor = null
     }
@@ -120,13 +150,13 @@ function render() {
     $("#welcome").append("<h4 id='timer'>Time left: </h4>")
     $("timer").show();
     $("#welcome").append("<hr>")
-    $("h1").text(`question #${++quesInc+1}:`)
+    $("h1").text(`question #${++quesInc + 1}:`)
     if (quesInc >= questionList.length) {
         endGame();
     }
- 
+
     $("h3").text(questionList[quesInc].question)
     displayOptions(quesInc)
-    number = 16;
+    number = 31;
     run();
 };
